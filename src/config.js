ea67caf9370928eca0dbf5218f03e45233159023
erickiki <ikquisbert@gmail.com>
2022-03-03 16:47:10 -0400

    node api rest

--- src/config.js
+++ src/config.js
@@ -1,6 +1,13 @@
 import { config as dotenv } from "dotenv";
 dotenv();
 
+/* export const config = {
+  host: process.env.DB_HOST || "localhost",
+  user: process.env.DB_USER || "root",
+  password: process.env.DB_PASSWORD || "",
+  database: process.env.DB_DATABASE || "soboce",
+  port: process.env.DB_PORT ||"3306"
+}; */
 export const config = {
   host: process.env.DB_HOST || "34.135.111.10",
   user: process.env.DB_USER || "dbsoboce",
